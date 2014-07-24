1922f7270cd477836329cd02db4395b8223c1995
Peter Desnoyers <pjd@ccs.neu.edu>
2014-07-24 17:09:28 -0400

    looks halfway-reasonable now

--- tests/unit/cli.py
+++ tests/unit/cli.py
@@ -26,43 +26,62 @@ class FakeResponse:
         FakeResponse.url = url
         FakeResponse.data = data
 
-    def reset(self):
-        FakeResponse.method = None
-        FakeResponse.url = None
-        FakeResponse.data = None
-
-    def check(self, method, url, values):
+    @staticmethod
+    def check(method, url, values):
         assert FakeResponse.method == method
         assert FakeResponse.url == url
-        for key,value in values:
-            assert FakeResponse.data[key] == value
-
+        if values == None:
+            assert FakeResponse.data == None
+        else:
+            for key,value in values:
+                assert FakeResponse.data[key] == value
+            
 @pytest.fixture(autouse=True)
 def no_requests(monkeypatch):
-    monkeypatch.setattr("requests.put", lambda url,data: FakeResponse('PUT', url, data))
-    monkeypatch.setattr("requests.delete", lambda url: FakeResponse('DELETE', url, None))
-    monkeypatch.setattr("requests.post", lambda url,data: FakeResponse('POST', url, data))
-    FakeResponse.reset()
+    monkeypatch.setattr("requests.put",
+                        lambda url,data: FakeResponse('PUT', url, data))
+    monkeypatch.setattr("requests.post",
+                        lambda url,data: FakeResponse('POST', url, data))
+    monkeypatch.setattr("requests.delete",
+                        lambda url: FakeResponse('DELETE', url, None))
+    monkeypatch.setattr("requests.post",
+                        lambda url,data: FakeResponse('POST', url, data))
 
 # and now the tests. Note that these only test that (a) the cli functions don't
 # crash, and (b) the http parameters match what we expect.
 #
+
 class TestCLI:
     def test_user_create(self):
         cli.user_create('joe', 'password')
-        FakeResponse.check('PUT', 'http://abc:5000/user/joe', [('password', 'password')])
+        FakeResponse.check('PUT', 'http://abc:5000/user/joe', 
+                           [('password', 'password')])
+    
+    def test_user_delete(self):
+        cli.user_delete('joe')
+        FakeResponse.check('DELETE', 'http://abc:5000/user/joe', [])
 
-        def test_network_create(self):
-            cli.network_create('net10', 'group1')
-            FakeResponse.check('PUT', 'http://abc:5000/network/net10', [('group', 'group1')])
+    def test_network_create(self):
+        cli.network_create('net10', 'group1')
+        FakeResponse.check('PUT', 'http://abc:5000/network/net10',
+                           [('group', 'group1')])
+    
+    def test_network_delete(self):
+        cli.network_delete('net10')
+        FakeResponse.check('DELETE', 'http://abc:5000/network/net10', [])
+        pass
 
-def network_delete(network):
-    pass
+    def test_group_add_user(self):
+        cli.group_add_user('group12', 'user4')
+        FakeResponse.check('POST', 'http://abc:5000/group/group12/add_user',
+                           [('user', 'user4')])
 
-def user_delete(username):
-    pass
-def group_add_user(group, user):
-
-def group_remove_user(group, user):
-
-def project_create(projectname, group, *args):
+    def test_group_remove_user(self):
+        cli.group_remove_user('group17', 'user42')
+        FakeResponse.check('POST', 'http://abc:5000/group/group17/remove_user',
+                           [('user', 'user42')])
+        
+    def test_project_create(self):
+        cli.project_create('projectname12', 'group2')
+        FakeResponse.check('PUT', 'http://abc:5000/project/projectname12',
+                           [('group', 'group2')])
